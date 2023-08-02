4738123255da75dcb39809746fe0aa0d04491d6b
Jaskirat-singh04 <83869412+Jaskirat-singh04@users.noreply.github.com>
2023-08-02 14:33:08 +0530

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,11 @@
+import base64
+
+# encoded_data = base64.b64encode("Encode this text")
+
+# print("Encoded text with base 64 is")
+# print(encoded_data)
+decoded_data = base64.b64decode("WFhWX1FKR18wMQ==")
+
+print("decoded text is ")
+print("-->" ,decoded_data)
+print(decoded_data)
